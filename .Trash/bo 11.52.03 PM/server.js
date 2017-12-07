0e54a1458104f3a199bf924cbb65c2d6e8d0bc44
BOHYEON AN <bohyeonan@bohyeons-mbp.attlocal.net>
2017-12-07 01:44:48 -0800

    bohyeon test

---
+++ .Trash/bo 11.52.03 PM/server.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var env = require('dotenv').load();
+var exphbs = require('express-handlebars') // set up view
+var path = require('path');
+var publicPath =  path.join(__dirname, './app/public');
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/', express.static(publicPath));
+
+// For Passport
+
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+
+app.use(passport.initialize());
+
+app.use(passport.session()); // persistent login sessions
+
+//For Handlebars
+app.set('views', './app/views')
+// app.engine("hbs", exphbs({ defaultLayout: "main" }));
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+
+app.get('/', function(req, res) {
+
+    // res.send('Welcome to Passport with Sequelize');
+res.render('signin.hbs');
+});
+//test
+app.get('/home', function(req, res) {
+  res.render('main.hbs');
+})
+
+app.get('/signin',function(req, res) {
+  res.render('form.hbs');
+});
+
+app.get('/form', function(req, res) {
+  user.all(function(data) {
+    var userObject = {user: data}
+    console.log(userObject);
+    res.render('form', userObject);
+  })
+})
+//Models
+var models = require("./app/models");
+
+// import this route in our server.js and pass app as an argument.
+//Routes
+var authRoute = require('./app/routes/auth.js')(app,passport);
+
+//load passport strategies : below route import
+require('./app/config/passport/passport.js')(passport, models.user);
+
+//Sync Database
+models.sequelize.sync().then(function() {
+
+    console.log('Nice! Database looks fine')
+
+}).catch(function(err) {
+
+    console.log(err, "Something went wrong with the Database Update!")
+
+});
+
+
+
+app.listen(8080, function(err) {
+
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+
+});
